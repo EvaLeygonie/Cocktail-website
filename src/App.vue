76b609f37810ba808cfdeaf1d7a1080d87826d3d
Eva Leygonie <eva.leygonie@gmail.com>
2025-02-18 18:41:20 +0100

    Cleaned upp code
    Ready to submit. Gonna try to fix a filtered search.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
-<script>
-  // import CocktailRandom from "./components/CocktailRandom.vue";
-
-  export default {
-    components: {
-      // CocktailRandom,
-    }
-  };
-</script>
-
 <template>
+  <nav>
+    <RouterLink to="/">Randomize</RouterLink>
+    <h1>Cocktails!</h1>
+    <RouterLink to="/search">Search drink</RouterLink>
+  </nav>
 
-    <nav>
-      <RouterLink to="/">Randomize</RouterLink>
-      <h1>Cocktails!</h1>
-      <RouterLink to="/search">Search drink</RouterLink>
-    </nav>
-
-
-<main>
-  <RouterView />
-</main>
+  <main>
+    <RouterView />
+  </main>
 </template>
